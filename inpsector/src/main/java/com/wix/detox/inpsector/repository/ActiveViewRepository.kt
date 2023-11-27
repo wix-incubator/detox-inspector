590af499a14ff408995fa481fe400df39ffd62a4
Georgy Steshin <georgys@wix.com>
2023-11-27 12:49:53 +0200

    Fixed stackoverflow

--- inpsector/src/main/java/com/wix/detox/inpsector/repository/ActiveViewRepository.kt
+++ inpsector/src/main/java/com/wix/detox/inpsector/repository/ActiveViewRepository.kt
@@ -13,7 +13,28 @@ data class ViewNode(
     val className: String,
     val parent: ViewNode?,
     val children: List<ViewNode>
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other !is ViewNode) {
+            return false
+        }
+        // All fields are immutable, so we can compare them directly except for the parent
+        // We don't compare the parent because it would cause a stack overflow
+        return id == other.id &&
+                tag == other.tag &&
+                label == other.label &&
+                width == other.width &&
+                height == other.height &&
+                x == other.x &&
+                y == other.y &&
+                className == other.className &&
+                children == other.children
+    }
+
+    override fun toString(): String {
+        return "ViewNode(id='$id', tag='$tag', label='$label', width=$width, height=$height, x=$x, y=$y, className='$className', children=$children)"
+    }
+}
 
 interface ActiveViewRepository {
 
